ca85a929f50ff1b357264d3fe9b790ed9d38451b
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2016-02-20 12:26:19 +0000

    form validations

--- resources/views/professeur/advert/createStep1.blade.php
+++ resources/views/professeur/advert/createStep1.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
 
 @section('content')
+    {!! HTML::script("js/parsley.min.js")!!}
 
     @if(isset($checkedSubjects))
-        {!! Form::open(['url' => "/modifier-annonce-1/{$advert_id}"]) !!}
+        <form method="POST" action="/modifier-annonce-1" accept-charset="UTF-8" data-parsley-validate id="subject_form">
         {!! Form::hidden('advert_id', $advert_id) !!}
     @else
-        {!! Form::open(['url' => '/nouvelle-annonce-1']) !!}
+        <form method="POST" action="/nouvelle-annonce-1" accept-charset="UTF-8" data-parsley-validate id="subject_form">
     @endif
+            {!! csrf_field() !!}
 
-        <div class="col-md-6 col-md-offset-3">
+            <div class="col-md-6 col-md-offset-3">
 
             <h2>Quelle(s) matière(s) enseignez-vous ?</h2>
 
@@ -26,7 +28,7 @@
                         @foreach ($subject->subSubjects as $subs)
                             <div class="ck-button">
                                 <input class="no-display" type="checkbox" name="subjects[]" id="{{$subs->id}}"
-                                       value="{{$subs->id}}"
+                                       value="{{$subs->id}} " data-parsley-required
                                        @if(isset($checkedSubjects) and in_array($subs->id, $checkedSubjects))
                                        checked
                                         @endif
@@ -41,8 +43,12 @@
                 </div>
             @endforeach
 
-            <div class="col-md-12 text-center">
 
+            <div class="col-md-12 text-center">
+                <div class="bs-callout bs-callout-warning hidden">
+                    <h4>Erreur :-(</h4>
+                    <p>Veuillez sélectionner une matière avant de passer à l'étape suivante.</p>
+                </div>
                 <button type="submit" class="button button-3d button-large button-rounded button-green">
                     J'ai sélectionné les matières de mon annonce
                 </button>
@@ -50,4 +56,18 @@
         </div>
 
         {!! Form::close() !!}
+
+        <script type="text/javascript">
+                $(function () {
+                    $('#subject_form').parsley().on('field:validated', function() {
+
+                                var ok = $('.parsley-error').length === 0;
+
+                                $('.bs-callout-warning').toggleClass('hidden', ok);
+                            })
+                            .on('form:submit', function() {
+                                return true; // Don't submit form for this demo
+                            });
+                });
+    </script>
 @endsection
