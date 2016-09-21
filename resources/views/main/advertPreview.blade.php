e527c1d490718d990f2dc893c92a81ea74298de6
Goshine <greenlig@gmail.com>
2016-09-21 18:32:46 +0100

    feat(Front end): Implementing addition form fields

--- resources/views/main/advertPreview.blade.php
+++ resources/views/main/advertPreview.blade.php
@@ -1,32 +1,27 @@
 <div class="row clearfix">
-    <div  class="component-profile-sidebar">
-      <div class="profile-author-profile">
-        <h3><a href="/{{$advert->slug}}">{{ \App\Models\User::find($advert->user_id)->firstname}}</a></h3>
-        <a href="/{{$advert->slug}}"><img src="{{ getAvatar($advert->user_id) }}" alt=""/></a>
-      </div>
+
+  <div  class="component-profile-sidebar">
+    <div class="profile-author-profile">
+      <h3><a href="/{{$advert->slug}}">{{ \App\Models\User::find($advert->user_id)->firstname}}</a></h3>
+      <a href="/{{$advert->slug}}"><img src="{{ getAvatar($advert->user_id) }}" alt=""/></a>
     </div>
+  </div>
 
 
-    <div class="profile-author-description">
-      <h5>{{ $advert->title }}</h5>
-      <div id="presentation"> {{ str_limit($advert->presentation, 345) }}</div>
-      <div class="">
-        <i class=""></i><strong>{{ $advert->location_city }}</strong>
-        @if(isset($distances) and isset($distances[$advert->id]))
-        <i>, à {{ round($distances[$advert->id],1) }} km</i>
-        @endif
-      </div>
-    </div>
+  <div class="profile-author-description">
+    <h5><a href="/{{$advert->slug}}"> {{ $advert->title }}</a></h5>
 
-    <div class="">
-      <div id="rightside">
-        <div id="info-price">
-          <div class="entry-overlay-meta">
-            <h3><a href="/{{$advert->slug}}">{{$advert->price}} Dhs/h</a></h3>
-            <h4>Premier cours offert !</h4>
-            <a href="/{{$advert->slug}}"> Voir l'annonce </a>
-        </div>
-      </div>
+    <div id="presentation"> {{ str_limit($advert->presentation, 345) }}</div>
+    <p>
+      <i class=""></i><strong>{{ $advert->location_city }}</strong>
+      @if(isset($distances) and isset($distances[$advert->id]))
+      <i>, à {{ round($distances[$advert->id],1) }} km</i>
+      @endif
+    </p>
+    <h3 class="info-price" ><a href="/{{$advert->slug}}">{{$advert->price}} Dhs/h</a></h3>
+    <div class="entry-overlay-meta">
+      <h4>Premier cours offert !</h4>
+      <a href="/{{$advert->slug}}"> Voir l'annonce </a>
     </div>
   </div>
 </div>
