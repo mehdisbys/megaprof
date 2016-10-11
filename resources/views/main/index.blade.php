5400df4bbae065d9d4d00b510ccf749e3bfd0805
Goshine <greenlig@gmail.com>
2016-10-11 22:51:57 +0100

    refactor(Work in progress on the avert view page):

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,125 +1,125 @@
 @extends('layouts.master')
 @section('content')
-    {!! HTML::script("js/awesomplete/awesomplete.min.js")!!}
-    {!! HTML::style("temp-css/awesomplete.css") !!}
-    {!! HTML::script('https://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places&amp;language=fr-FR&amp;key=AIzaSyBMbqBykgfCFr3pgcj0dRU6rlmSggAZygc') !!}
-    {!! HTML::script("js/locationpicker.jquery.js") !!}
-    {!! HTML::script("js/jquery.geocomplete.min.js") !!}
-    {!! HTML::script("js/jquery.form.min.js") !!}
-    <div class="home-search">
-        <h2> <span id="search_result_text">
-      @if(isset($selectedSubject))
-                    {{$adverts->total() > 0 ? $adverts->total():'Aucun '}} Résultat{{$adverts->total() > 1 ? 's' : ''}}
-                    pour {{$selectedSubject->name}}
+{!! HTML::script("js/awesomplete/awesomplete.min.js")!!}
+{!! HTML::style("temp-css/awesomplete.css") !!}
+{!! HTML::script('https://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places&amp;language=fr-FR&amp;key=AIzaSyBMbqBykgfCFr3pgcj0dRU6rlmSggAZygc') !!}
+{!! HTML::script("js/locationpicker.jquery.js") !!}
+{!! HTML::script("js/jquery.geocomplete.min.js") !!}
+{!! HTML::script("js/jquery.form.min.js") !!}
 
-                    @if(isset($selectedCity))
-                        {{ " à " . $selectedCity}}
-                    @endif
-
-                @endif
-    </span></h2>
-        <div class="home-search-form-inner autocomplete awesomplete">
-            <div class="">
-                <form action="/search" method="post" id="search_form2">
-                    {!! csrf_field() !!}
-                    <div class="home-search-field-wrapper">
-                        <input
-                                id="subject_input"
-                                class="awesomplete home-search-input autocomplete-input-subject"
-                                placeholder="Que souhaitez-vous apprendre ?"
-                                data-minchars="1"
-                                data-autofirst="1"
-                                data-list="{!! $subsubjects !!}"
-                                name="subject"
-                                type="text"
-                                autocomplete="off"
-                                aria-autocomplete="list"
-                                value="{{$selectedSubject->name or ''}}"/>
-                    </div>
-                    <div class="home-search-field-wrapper">
-                        <input id="location_input"
-                               class="home-search-input autocomplete-input-city"
-                               placeholder="Ville où le cours a lieu"
-                               name="city" type="text" value="{{$selectedCity or ''}}"/>
-                    </div>
-                    <div class="home-search-button-wrapper ">
-                        <button id="submit-btn" class="button home-search-submit" type="submit"> Chercher</button>
-                    </div>
-            </div>
+<div class="home-search">
+  <h2 id="search_result_text">
+    @if(isset($selectedSubject))
+    {{$adverts->total() > 0 ? $adverts->total():'Aucun '}} Résultat{{$adverts->total() > 1 ? 's' : ''}}
+    pour {{$selectedSubject->name}}
+    @if(isset($selectedCity))
+    {{ " à " . $selectedCity}}
+    @endif
+    @endif
+  </h2>
+  <div class="home-search-form-inner autocomplete awesomplete">
+    <div class="search-form-wrapper">
+      <form action="/search" method="post" id="search_form2">
+        {!! csrf_field() !!}
+        <div class="home-search-field-wrapper">
+          <input
+          id="subject_input"
+          class="awesomplete home-search-input autocomplete-input-subject"
+          placeholder="Que souhaitez-vous apprendre ?"
+          data-minchars="1"
+          data-autofirst="1"
+          data-list="{!! $subsubjects !!}"
+          name="subject"
+          type="text"
+          autocomplete="off"
+          aria-autocomplete="list"
+          value="{{$selectedSubject->name or ''}}"/>
         </div>
+
+        <div class="home-search-field-wrapper">
+          <input
+          id="location_input"
+          class="home-search-input autocomplete-input-city"
+          placeholder="Ville où le cours a lieu"
+          name="city" type="text" value="{{$selectedCity or ''}}"/>
+        </div>
+
+        <div class="home-search-button-wrapper ">
+          <button id="submit-btn" class="button home-search-submit" type="submit"> Chercher</button>
+        </div>
+
         <div class="radius-group">
-            <div id="radius_input" class="no-visibility">
-                <h3> Je peux me déplacer dans un rayon de </h3>
-                <div class="sorting-field">
-                    <label>Select radius</label>
-                    <input type="range"/>
-                </div>
-            </div>
-            <div id="teacher_gender" class="topmargin-sm">
-                <h3>Je préfère un professeur:</h3>
-
-                <label class="search-radio">
-                    <input name="gender" value="man" type="radio"> Homme
-                </label>
-
-                <label class="search-radio">
-                    <input name="gender" value="woman" type="radio"> Femme
-                </label>
-
-                <label class="search-radio">
-                    <input name="gender" value="both" type="radio"> Les deux me vont
-                </label>
-            </div>
-
-            <div class="location-details no-visibility">
-                {!! Form::hidden('lng',null, ['id' => 'longitude']) !!}
-                {!! Form::hidden('lat', null, ['id' => 'latitude']) !!}
-            </div>
-            <input type="hidden" class="autocomplete-current-page" value="{{$adverts->currentPage()}}" name="page">
-        </div>
-        </form>
-    </div>
-
-    <div class="section section-odd home-profs">
-        <div class="wrapper">
+          <div id="radius_input" class="no-visibility">
+            <h3> Je peux me déplacer dans un rayon de </h3>
             <div class="sorting-field">
-                <label>Trier par</label>
-                <select name="sortBy" class="autocomplete-input-sortby">
-                    <option value="distance">Distance</option>
-                    <option value="date">Date</option>
-                    <option value="price">Prix</option>
-                </select>
+              <label>Select radius</label>
+              <input type="range"/>
             </div>
-            <div class="home-profs-items-container">
-                <ul class="home-profs-items inline-block-grid filters-results"></ul>
-                <!-- optional to show advert count  -->
-                @if(count($adverts) == 0)
-                    <div id="search_results" class="">
-                        <div>Malheuresement aucune annonce correspondant à vos critères n'a été trouvée. Réessayez avec
-                            d'autres options
-                        </div>
-                    </div>
-                @else
-                    <div class="count_results" class="topmargin-sm bottommargin-sm">
-                        <span id="count_text">{{ $adverts->total() }} Annonce{{$adverts->total() > 1 ? 's' : ''}}
-                            trouvées</span>
-                    </div>
-                    <div id="search_results" class="">
-                        @include('main.multipleAdvertPreview')
-                    </div>
+          </div>
+          <div id="teacher_gender" class="topmargin-sm">
+            <h3>Je préfère un professeur:</h3>
+            <label class="search-radio">
+              <input name="gender" value="man" type="radio"> Homme
+            </label>
+            <label class="search-radio">
+              <input name="gender" value="woman" type="radio"> Femme
+            </label>
+            <label class="search-radio">
+              <input name="gender" value="both" type="radio"> Les deux me vont
+            </label>
+          </div>
 
-                @endif
-            </div>
-            <!-- optional to show advert count  -->
+          <div class="location-details no-visibility">
+            {!! Form::hidden('lng',null, ['id' => 'longitude']) !!}
+            {!! Form::hidden('lat', null, ['id' => 'latitude']) !!}
+          </div>
+
+          <input type="hidden" class="autocomplete-current-page" value="{{$adverts->currentPage()}}" name="page">
         </div>
+      </form>
     </div>
+  </div>
+</div>
+<div class="section section-odd home-profs">
+  <div class="wrapper">
+    <div class="sorting-field">
+      <label>Trier par</label>
+      <select name="sortBy" class="autocomplete-input-sortby">
+        <option value="distance">Distance</option>
+        <option value="date">Date</option>
+        <option value="price">Prix</option>
+      </select>
+    </div>
+    <div class="home-profs-items-container">
+      <ul class="home-profs-items inline-block-grid filters-results"></ul>
+      <!-- optional to show advert count  -->
+      @if(count($adverts) == 0)
+      <div id="search_results" class="">
+        <div>Malheuresement aucune annonce correspondant à vos critères n'a été trouvée. Réessayez avec
+          d'autres options
+        </div>
+      </div>
+      @else
+      <div class="count_results" class="topmargin-sm bottommargin-sm">
+        <span id="count_text">{{ $adverts->total() }} Annonce{{$adverts->total() > 1 ? 's' : ''}}
+          trouvées</span>
+      </div>
+      <div id="search_results" class="">
+        @include('main.multipleAdvertPreview')
+      </div>
 
-    <script>
-        /**
-         * TODO -- refactor javascript
-         * there is a bit too much going on here
-         *
-         * */
+      @endif
+    </div>
+    <!-- optional to show advert count  -->
+  </div>
+</div>
+
+<script>
+  /**
+   * TODO -- refactor javascript
+   * there is a bit too much going on here
+   *
+   * */
         $(document).ready(function () {
 
             function updatePage(data) {
@@ -136,47 +136,45 @@
                     searchText = data.count + ' Résultats pour ' + data.params.selectedSubject;
                 }
 
-                if (data.params.city) {
-                    searchText += ' à ' + data.params.city;
-                }
+  if (data.params.city) {
+    searchText += ' à ' + data.params.city;
+  }
 
-                $("#search_result_text").html(searchText);
+$("#search_result_text").html(searchText);
 
             }
 
-            var sendFormBy = function sortBySendForm(page) {
+var sendFormBy = function sortBySendForm(page) {
 
-                        function sendForm(event) {
-                            event.preventDefault();
-                            var subject = $(".autocomplete-input-subject").val();
-                            var city = $(".autocomplete-input-city").val();
-                            var token = $("[name='_token']").val();
-                            var sortBy = $(".autocomplete-input-sortby").val();
-                            var gender = $("[name='gender']:checked").val();
+  function sendForm(event) {
+    event.preventDefault();
+    var subject = $(".autocomplete-input-subject").val();
+    var city = $(".autocomplete-input-city").val();
+    var token = $("[name='_token']").val();
+    var sortBy = $(".autocomplete-input-sortby").val();
+    var gender = $("[name='gender']:checked").val();
 
-                            $.post('/search',
-                                    {
-                                        'subject': subject,
-                                        'city': city,
-                                        '_token': token,
-                                        'sortBy': sortBy,
-                                        'gender': gender,
-                                        'page': page ? page.replace(/[^0-9]/g,'') : null
-                                    },
-                                    function (data) {
-                                        updatePage(data);
-                                    });
-                        };
-                        return sendForm;
-                    }
+    $.post('/search',
+      {
+        'subject': subject,
+        'city': city,
+        '_token': token,
+        'sortBy': sortBy,
+        'gender': gender,
+        'page': page ? page.replace(/[^0-9]/g,'') : null
+      },
+      function (data) {
+        updatePage(data);
+      });
+  };
+  return sendForm;
+}
 
-            $(document).on('click', '.home-search-submit', sendFormBy(null));
-            $(".autocomplete-input-sortby").change(sendFormBy(null));
-            $(document).on("click", '.pagination-link', function(event) {sendFormBy($(this).attr('href'))(event);});
-
-
-            // Geocompletion
-            $('#location_input').geocomplete({types: ['(cities)'], details: ".location-details",});
-        });
-    </script>
+$(document).on('click', '.home-search-submit', sendFormBy(null));
+  $(".autocomplete-input-sortby").change(sendFormBy(null));
+  $(document).on("click", '.pagination-link', function(event) {sendFormBy($(this).attr('href'))(event);});
+  // Geocompletion
+    $('#location_input').geocomplete({types: ['(cities)'], details: ".location-details",});
+  });
+</script>
 @endsection
--- resources/views/professeur/advert/view.blade.php
+++ resources/views/professeur/advert/view.blade.php
@@ -1,163 +1,142 @@
-@extends('layouts.__master')
+@extends('layouts.master')
 
 @section('content')
 
-    <div class="row" data-spy="scroll" data-target=".scrollspy">
-
-        <div id="author" class="col-md-3 col-md-offset-1 scrollspy">
-            <div id="leftside" data-spy="affix">
-                <img src="{{ $advert->getAvatar() }}" alt="">
-
-                <div id="info-author">
-                    <div class="entry-overlay-meta">
-                        <h3><a href="#" class=" center">{{$advert->user->firstname }}</a></h3>
-                        <div class="clearfix"></div>
-                        <ul class="iconlist">
-                            <li><i class="icon-location"></i> <strong>{{ $advert->location_city }}</strong> </li>
-                            <li><i class="icon-study green"></i> <strong>Diplôme vérifié</strong></li>
-                            <li><i class="icon-check green"></i> <strong>Coordonnées vérifiées</strong></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+<div class="row" data-spy="scroll" data-target=".scrollspy">
+  <div id="author" class="col-md-3 col-md-offset-1 scrollspy">
+    <div id="leftside" data-spy="affix">
+      <img src="{{ $advert->getAvatar() }}" alt="avatar">
+      <div id="info-author">
+        <div class="entry-overlay-meta">
+          <h3><a href="#" class=" center">{{$advert->user->firstname }}</a></h3>
+          <div class="clearfix"></div>
+          <ul class="iconlist">
+            <li><i class="icon-location"></i> <strong>{{ $advert->location_city }}</strong> </li>
+            <li><i class="icon-study green"></i> <strong>Diplôme vérifié</strong></li>
+            <li><i class="icon-check green"></i> <strong>Coordonnées vérifiées</strong></li>
+          </ul>
         </div>
-
-        <div class="col-md-5">
-            <h3>{{ $advert->title }}</h3>
-
-            <div id="presentation"> {{ $advert->presentation  }}</div>
-
-            <div class="divider divider-center"><i class="icon-circle"></i></div>
-
-            <div class="">
-                <h4 id="experience-title" class="center experience-title">Expérience</h4>
-                <div id="experience"> {{ $advert->experience }}</div>
-            </div>
-
-            <div class="divider divider-center"><i class="icon-circle"></i></div>
-
-            <div class="">
-                <h4 id="curriculum-title" class="center curriculum-title">Curriculum Vitae</h4>
-                <div id="curriculum"> {{ $advert->content }}</div>
-            </div>
-        </div>
-
-        <div class="col-md-3 scrollspy">
-            <div id="rightside" data-spy="affix" class="">
-
-                <div id="info-price" >
-                    <div class="entry-overlay-meta">
-                        <h3><a href="#" class=" center"> {{$advert->price}} Dhs/h</a></h3>
-
-                        <h4>Premier cours offert !</h4>
-
-                        <div class="col-md-8">
-                            <a class="btn btn-danger btn-block btn-md" href="/mise-en-relation/{{$advert->id}}">Réserver un cours</a>
-                        </div>
-                        <div class="col-md-10 topmargin-sm">
-                            <a href="http://www.facebook.com/sharer.php?u=http://localhost:8000/{{$advert->slug}}" data-send="false" data-layout="box_count" data-width="60" data-show-faces="false" rel="nofollow" target="_blank" class="social-icon si-colored si-facebook">
-                                <i class="icon-facebook"></i>
-                                <i class="icon-facebook"></i>
-                            </a>
-
-                            <a href="http://twitter.com/share" data-count="vertical" rel="nofollow" target="_blank" class="social-icon si-colored si-twitter">
-                                <i class="icon-twitter"></i>
-                                <i class="icon-twitter"></i>
-                            </a>
-
-                            <a href="#" class="social-icon si-colored si-email3"  >
-                                <i class="icon-email3"></i>
-                                <i class="icon-email3"></i>
-                            </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
+
+  <div class="col-md-5">
+    <h3>{{ $advert->title }}</h3>
+
+    <div id="presentation"> {{ $advert->presentation  }}</div>
+    <div class="divider divider-center"><i class="icon-circle"></i></div>
+    <div class="">
+      <h4 id="experience-title" class="center experience-title">Expérience</h4>
+      <div id="experience"> {{ $advert->experience }}</div>
+    </div>
+    <div class="divider divider-center"><i class="icon-circle"></i></div>
+    <div class="">
+      <h4 id="curriculum-title" class="center curriculum-title">Curriculum Vitae</h4>
+      <div id="curriculum"> {{ $advert->content }}</div>
+    </div>
+  </div>
+  <div class="col-md-3 scrollspy">
+    <div id="rightside" data-spy="affix" class="">
+      <div id="info-price" >
+        <div class="entry-overlay-meta">
+          <h3><a href="#" class=" center"> {{$advert->price}} Dhs/h</a></h3>
+          <h4>Premier cours offert !</h4>
+          <div class="col-md-8">
+            <a class="btn btn-danger btn-block btn-md" href="/mise-en-relation/{{$advert->id}}">Réserver un cours</a>
+          </div>
+          <div class="col-md-10 topmargin-sm">
+            <a href="http://www.facebook.com/sharer.php?u=http://localhost:8000/{{$advert->slug}}" data-send="false" data-layout="box_count" data-width="60" data-show-faces="false" rel="nofollow" target="_blank" class="social-icon si-colored si-facebook">
+              <i class="icon-facebook"></i>
+              <i class="icon-facebook"></i>
+            </a>
+            <a href="http://twitter.com/share" data-count="vertical" rel="nofollow" target="_blank" class="social-icon si-colored si-twitter">
+              <i class="icon-twitter"></i>
+              <i class="icon-twitter"></i>
+            </a>
+            <a href="#" class="social-icon si-colored si-email3"  >
+              <i class="icon-email3"></i>
+              <i class="icon-email3"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix"></div>
+<div class="col-md-5 col-md-offset-4 topmargin-sm table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Chez {{$advert->user->firstname}}</th>
+        <th>À domicile</th>
+        <th>Par webcam</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1 heure</td>
+        <td>{{$advert->price}}Dh</td>
+        <td>{{$advert->price_travel}}Dh</td>
+        <td>{{$advert->price * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
+      </tr>
+      <tr>
+        <td>5 heures</td>
+        <td>{{$advert->price_5_hours}}Dh</td>
+        <td>{{$advert->price_5_hours + $advert->price_travel}}Dh</td>
+        <td>{{$advert->price_5_hours * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
+      </tr>
+      <tr>
+        <td>10 heures</td>
+        <td>{{$advert->price_10_hours}}Dh</td>
+        <td>{{$advert->price_10_hours + $advert->price_travel}}Dh</td>
+        <td>{{$advert->price_10_hours * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="col-md-7 col-md-offset-4 topmargin-sm">
+
+  <h3>Avis des étudiants</h3>
+
+  @foreach($comments as $comment)
+  <div class="col-md-12">
+    <strong>{{$comment->sourceUser->firstname}}</strong> : {{$comment->comment}}
+  </div>
+  @endforeach
+</div>
+
+<div class="col-md-7 col-md-offset-4 topmargin-sm">
+
+  <h3>Les professeurs similaires</h3>
+  <div class="clearfix"></div>
+  @foreach($similarAdverts as $advert)
+
+  <div class="col-md-4">
+    <div class="col-md-6">
+      <img class="avatar" src="{{ getAvatar($advert->user_id) }}" alt=""/>
+    </div>
+    <strong>{{ \App\Models\User::find($advert->user_id)->firstname}}</strong> : <h5><a href="/{{$advert->slug}}"> {{ $advert->title }}</a></h5>
     <div class="clearfix"></div>
+    <i class="icon-location"></i> <strong>{{ $advert->location_city }}</strong>
 
-    <div class="col-md-5 col-md-offset-4 topmargin-sm table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th></th>
-                <th>Chez {{$advert->user->firstname}}</th>
-                <th>À domicile</th>
-                <th>Par webcam</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>1 heure</td>
-                <td>{{$advert->price}}Dh</td>
-                <td>{{$advert->price_travel}}Dh</td>
-                <td>{{$advert->price * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
-            </tr>
-            <tr>
-                <td>5 heures</td>
-                <td>{{$advert->price_5_hours}}Dh</td>
-                <td>{{$advert->price_5_hours + $advert->price_travel}}Dh</td>
-                <td>{{$advert->price_5_hours * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
-            </tr>
-            <tr>
-                <td>10 heures</td>
-                <td>{{$advert->price_10_hours}}Dh</td>
-                <td>{{$advert->price_10_hours + $advert->price_travel}}Dh</td>
-                <td>{{$advert->price_10_hours * ((100 - $advert->price_webcam_percentage)/100)}}Dh</td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
+  </div>
+  @endforeach
+</div>
 
-    <div class="col-md-7 col-md-offset-4 topmargin-sm">
+<script>
+  $( document ).ready(function() {
+    $("#leftside").affix({
+      offset: {
+        top: $("#leftside").offset().top,
+        bottom: ($('#validate_buttons').outerHeight(true) + $('#footer').outerHeight(true)) + 100}});
 
-        <h3>Avis des étudiants</h3>
-
-        @foreach($comments as $comment)
-            <div class="col-md-12">
-                <strong>{{$comment->sourceUser->firstname}}</strong> : {{$comment->comment}}
-            </div>
-        @endforeach
-    </div>
-
-    <div class="col-md-7 col-md-offset-4 topmargin-sm">
-
-        <h3>Les professeurs similaires</h3>
-        <div class="clearfix"></div>
-        @foreach($similarAdverts as $advert)
-
-            <div class="col-md-4">
-                <div class="col-md-6">
-                    <img class="avatar" src="{{ getAvatar($advert->user_id) }}" alt=""/>
-                </div>
-                <strong>{{ \App\Models\User::find($advert->user_id)->firstname}}</strong> : <h5><a href="/{{$advert->slug}}"> {{ $advert->title }}</a></h5>
-                <div class="clearfix"></div>
-               <i class="icon-location"></i> <strong>{{ $advert->location_city }}</strong>
-
-            </div>
-        @endforeach
-    </div>
-
-    <script>
-
-        $( document ).ready(function() {
-
-            $("#leftside").affix({
-                offset: {
-                    top: $("#leftside").offset().top,
-                    bottom: ($('#validate_buttons').outerHeight(true) + $('#footer').outerHeight(true)) + 100}});
-
-            $("#rightside").affix({
-                offset: {
-                    top: $("#rightside").offset().top,
-                    bottom: ($('#validate_buttons').outerHeight(true) + $('#footer').outerHeight(true)) + 140}});
-
-        });
-
-    </script>
-
-
-
+    $("#rightside").affix({
+      offset: {
+        top: $("#rightside").offset().top,
+        bottom: ($('#validate_buttons').outerHeight(true) + $('#footer').outerHeight(true)) + 140}});
+  });
+</script>
 @endsection
