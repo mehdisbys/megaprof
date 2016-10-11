73550ca8aba05f70131f1f5eeb0890e754e738e1
Goshine <greenlig@gmail.com>
2016-10-11 22:50:48 +0100

    fix(Advert view): Fixed advert view layout issues

--- resources/views/main/advertPreview.blade.php
+++ resources/views/main/advertPreview.blade.php
@@ -1,24 +1,32 @@
-<div class="row clearfix">
-
+<div class="temp-row">
   <div  class="component-profile-sidebar">
     <div class="profile-author-profile">
-      <h3><a href="/{{$advert->slug}}">{{ \App\Models\User::find($advert->user_id)->firstname}}</a></h3>
-      <a href="/{{$advert->slug}}"><img class="avatar" src="{{ getAvatar($advert->user_id) }}" alt=""/></a>
+      <h3>
+        <a href="/{{$advert->slug}}">{{ \App\Models\User::find($advert->user_id)->firstname}}</a>
+      </h3>
+      <a href="/{{$advert->slug}}">
+        <img class="avatar" src="{{ getAvatar($advert->user_id) }}" alt="avatar"/>
+      </a>
     </div>
   </div>
-
-
   <div class="profile-author-description">
-    <h5><a href="/{{$advert->slug}}"> {{ $advert->title }}</a></h5>
+    <h5>
+      <a href="/{{$advert->slug}}"> {{ $advert->title }}</a>
+    </h5>
 
     <div id="presentation"> {{ str_limit($advert->presentation, $trimChar ?? 345) }}</div>
+
     <p>
-      <i class=""></i><strong>{{ $advert->location_city }}</strong>
+      <strong>{{ $advert->location_city }}</strong>
       @if(isset($distances) and isset($distances[$advert->id]))
       <i>, à {{ round($distances[$advert->id],1) }} km</i>
       @endif
     </p>
-    <h3 class="info-price" ><a href="/{{$advert->slug}}">{{$advert->price}} Dhs/h</a></h3>
+
+    <h3 class="info-price" >
+      <a href="/{{$advert->slug}}">{{$advert->price}} Dhs/h</a>
+    </h3>
+
     <div class="entry-overlay-meta">
       <h4>Premier cours offert !</h4>
       <a href="/{{$advert->slug}}"> Voir l'annonce </a>
