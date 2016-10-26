a8de475381c79ef681d99db84800b7e257856837
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2016-10-26 14:44:30 +0100

    Update : Dashboard notifications will include only facebook for now

--- app/Listeners/DashboardNotificationsAfterAdSubmission.php
+++ app/Listeners/DashboardNotificationsAfterAdSubmission.php
@@ -21,6 +21,16 @@ class DashboardNotificationsAfterAdSubmission
 
         //Run through the ad and check if it can be made better
 
+        // TODO Social networks sharing
+        Notification::createAdvertNotification(
+            'social-networks',
+            "Touchez le maximum d'élèves potentiels en faisant la promotion de votre annonce : " . str_limit($event->advert->title, 55),
+            "",
+            $advertId,
+            \Auth::id()
+        );
+
+
         // Webcam
         if($event->advert->can_webcam == NULL)
         {
@@ -32,54 +42,43 @@ class DashboardNotificationsAfterAdSubmission
                 \Auth::id());
         }
 
-        // TODO Presentation video
-        if($event->advert->marketing_video == NULL)
-        {
-            Notification::createAdvertNotification(
-                'marketing-video',
-                config('notifications.marketing-video'),
-                "",
-                $advertId,
-                \Auth::id());
-        }
-
-        // TODO Social networks sharing
-            Notification::createAdvertNotification(
-                'social-networks',
-                "Touchez le maximum d'élèves potentiels en faisant la promotion de votre annonce : " . str_limit($event->advert->title, 55),
-                "",
-                $advertId,
-                \Auth::id()
-            );
-
-        // TODO Check degree has been sent to us
-
-        Notification::createAdvertNotification(
-            'degree-check',
-            config('notifications.degree-check'),
-            "",
-            $advertId,
-            \Auth::id()
-        );
-
-        // TODO Small groups courses
-        Notification::createAdvertNotification(
-            'group-course',
-            config('notifications.group-course'),
-            "",
-            $advertId,
-            \Auth::id()
-        );
-
-        // TODO Try professional offer for teachers
-
-        Notification::createAdvertNotification(
-            'pro-offer',
-            config('notifications.pro-offer'),
-            "",
-            $advertId,
-            \Auth::id()
-        );
+//        // TODO Presentation video
+//        if($event->advert->marketing_video == NULL)
+//        {
+//            Notification::createAdvertNotification(
+//                'marketing-video',
+//                config('notifications.marketing-video'),
+//                "",
+//                $advertId,
+//                \Auth::id());
+//        }
+//
+//        // TODO Check degree has been sent to us
+//        Notification::createAdvertNotification(
+//            'degree-check',
+//            config('notifications.degree-check'),
+//            "",
+//            $advertId,
+//            \Auth::id()
+//        );
+//
+//        // TODO Small groups courses
+//        Notification::createAdvertNotification(
+//            'group-course',
+//            config('notifications.group-course'),
+//            "",
+//            $advertId,
+//            \Auth::id()
+//        );
+//
+//        // TODO Try professional offer for teachers
+//        Notification::createAdvertNotification(
+//            'pro-offer',
+//            config('notifications.pro-offer'),
+//            "",
+//            $advertId,
+//            \Auth::id()
+//        );
 
     }
 }
