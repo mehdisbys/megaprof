092bbe9aa2be148a1354c220f26ffaa5e3cd6688
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2016-11-14 20:01:18 +0000

    Update : UI enhancement for step 1

--- resources/views/professeur/partials/step1.blade.php
+++ resources/views/professeur/partials/step1.blade.php
@@ -15,9 +15,10 @@
                 {!! csrf_field() !!}
 
 
-                <div class="col-md-6 col-md-offset-3">
+                <div class="col-md-10 col-md-offset-2">
 
-                    <h2>Quelle(s) matière(s) enseignez-vous ?</h2>
+                    <h2 class="col-md-12">Quelle(s) matière(s) enseignez-vous ?</h2>
+                    <div class="col-md-8">
                     <input
                             id="subject_input"
                             class="sm-form-control bottommargin-sm col-md-12"
@@ -30,49 +31,57 @@
                             autocomplete="off"
                             aria-autocomplete="list"
                             data-multiple/>
+                    </div>
                     <div class="col-md-6 col-md-offset-3 bottommargin-sm">Ou choisissez-en ci-dessous</div>
 
+                    <div class="col-md-10 tabs side-tabs">
 
-                @foreach ($subjects as $subject)
-                        <div class="toggle toggle-bg col-md-6">
+                        <ul class="tab-nav tab-nav2" role="tablist">
+                            @foreach ($subjects as $subject)
+                                <li class="ui-state-default ui-corner-top"
+                                    role="tab" tabindex="-1" aria-selected="false" aria-expanded="false">
+                                    <a href="#subject-{{$subject->id}}" class="ui-tabs-anchor"
+                                       role="presentation" tabindex="-1"
+                                       id="ui-{{$subject->id}}">{{$subject->name}} ({{ $subject->subSubjects->count() }}
+                                        )</a></li>
+                            @endforeach
+                        </ul>
 
-                            <div class="togglet" style="background-color: transparent; border: 1px dashed black;">
-                                <i class="toggle-closed icon-ok-circle"></i>
-                                <i class="toggle-open icon-remove-circle"></i>
-                                {{$subject->name}} ({{ $subject->subSubjects->count() }})
-                            </div>
+                        <div class="tab-container">
 
+                            @foreach ($subjects as $subject)
 
-                            <div class="togglec" style="display: none;">
-                                @foreach ($subject->subSubjects as $subs)
-                                    <div class="">
-                                        <input class="" type="checkbox" name="subjects[]" id="{{$subs->id}}"
-                                               value="{{$subs->id}} "
-                                               @if(isset($checkedSubjects) and in_array($subs->id, $checkedSubjects))
-                                               checked
-                                                @endif
-                                        >
-                                        <label class="" for="{{$subs->id}}"
-                                               style="text-transform: none; font-weight: 100">
-                                            <span>{{$subs->name}}</span>
-                                        </label>
-                                    </div>
-                                @endforeach
-                            </div>
-
-                        </div>
+                                <div class="tab-content" id="subject-{{$subject->id}}">
+                                    @foreach ($subject->subSubjects->sortBy('name') as $subs)
+                                        <div class="col-md-6">
+                                            <input class="" type="checkbox" name="subjects[]" id="{{$subs->id}}"
+                                                   value="{{$subs->id}} "
+                                                   @if(isset($checkedSubjects) and in_array($subs->id, $checkedSubjects))
+                                                   checked
+                                                    @endif
+                                            >
+                                            <label class="" for="{{$subs->id}}"
+                                                   style="text-transform: none; font-weight: 100">
+                                                <span>{{$subs->name}}</span>
+                                            </label>
+                                        </div>
+                                    @endforeach
+                                </div>
                     @endforeach
-
-
-                    <div class="col-md-12 text-center">
-                        <div class="bs-callout bs-callout-warning hidden">
-                            <h4>Erreur :-(</h4>
-                            <p>Veuillez sélectionner une matière avant de passer à l'étape suivante.</p>
                         </div>
-                        <button type="submit" class="button button-3d button-large button-rounded button-green">
-                            J'ai sélectionné les matières de mon annonce
-                        </button>
                     </div>
+                </div>
+
+
+                <div class="col-md-12 text-center">
+                    <div class="bs-callout bs-callout-warning hidden">
+                        <h4>Erreur :-(</h4>
+                        <p>Veuillez sélectionner une matière avant de passer à l'étape suivante.</p>
+                    </div>
+                    <button type="submit" class="button button-3d button-large button-rounded button-green">
+                        J'ai sélectionné les matières de mon annonce
+                    </button>
+                </div>
                 </div>
 
                 {!! Form::close() !!}
