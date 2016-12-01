8115bfc63ebcc482a8697d090caa001b86e50341
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2016-12-01 15:26:23 +0000

    Update : amend comments display

--- resources/views/comments/pendingComments.blade.php
+++ resources/views/comments/pendingComments.blade.php
@@ -3,7 +3,6 @@
 
         <div class="content">
             <div class="col-md-12 border1px topmargin-sm content-message" id="{{$comment->id}}">
-                <button type="button" class="close">&times;</button>
                 <div class="col-md-8 topmargin-sm bottommargin-sm">
                     Laissez un commentaire sur votre
                     @if($comment->iWasTheProf())
@@ -14,7 +13,7 @@
                     <strong>{{$comment->targetUser->firstname}}</strong>
                 </div>
 
-                <div class="col-md-4 pull-right">
+                <div class="col-md-4 pull-right topmargin-sm bottommargin-sm">
                     <a class="btn btn-warning btn-md" href="/laisser-un-commentaire/{{$comment->id}}">
                         Commenter
                     </a>
