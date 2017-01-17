ae80c84f5c3dd0b64bd0fee88693cebbee13e199
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-01-17 14:32:21 +0000

    Add google analytics

--- resources/views/layouts/__master.blade.php
+++ resources/views/layouts/__master.blade.php
@@ -29,15 +29,6 @@
     <!-- Document Title
     ============================================= -->
     <title>Megaprof</title>
-    <script type="text/javascript" id="inspectletjs">
-        window.__insp = window.__insp || [];
-        __insp.push(['wid', 1483144060]);
-        (function() {
-            function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-            setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
-        })();
-    </script>
-
 
 </head>
 
@@ -220,5 +211,15 @@
 {!! HTML::script("js/functions.js") !!}
 
 @include('includes/toastr/toastr')
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-90487629-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
