e42da90feb9607cac8aa97a7e316be2e11fe64a3
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-03-21 14:06:37 +0000

    Add button to confirmation email

--- resources/views/emails/auth/confirmEmail.blade.php
+++ resources/views/emails/auth/confirmEmail.blade.php
@@ -1,20 +1,25 @@
 @extends('emails.master')
 @section('content')
 
-	<h2>{{$name}}, bienvenue sur TAELAM ! – Confirmation de votre inscription</h2>
-	<br>
+    <h2>{{$name}}, bienvenue sur TAELAM ! – Confirmation de votre inscription</h2>
+    <br>
 
-	<p>Bonjour {{$name}},</p>
+    <p>Bonjour {{$name}},</p>
 
-	<br>
+    <br>
 
-	<p>Pour commencer à bénéficier de nos services, merci de confirmer la création de votre en compte en cliquant <a href="{{$link}}">ici</a>.</p>
-	<p>Nous vous remercions de votre confiance,suivez-nous sur les réseaux sociaux pour être au courant de l'actualité Taelam.</p>
-	<br>
-	<p>
-	A très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
-	<br>
+    <p>Pour commencer à bénéficier de nos services, merci de confirmer la création de votre compte en vous cliquant sur le bouton suivant :</p>
+    <div class="" style="text-align: center">
+        <a href="{{$link}}" class="btn btn-success btn-lg ">Se Connecter</a>
+    </div>
+    <br>
+    <p>Nous vous remercions de votre confiance,suivez-nous sur les réseaux sociaux pour être au courant de l'actualité
+        Taelam.</p>
+    <br>
+    <p>
+        A très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
+    <br>
 
-	<p>L'Équipe Taelam</p>
+    <p>L'Équipe Taelam</p>
 
 @stop
