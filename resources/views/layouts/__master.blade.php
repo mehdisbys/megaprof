c6d191f38f7652bbd8dc843abf24c8f5c9cf92cf
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-03-25 16:22:08 +0000

    footer iteration

--- resources/views/layouts/__master.blade.php
+++ resources/views/layouts/__master.blade.php
@@ -118,81 +118,54 @@
 
     <!-- Footer
     ============================================= -->
-    <footer id="footer" class="dark">
-
+    <footer class="footer-div topmargin-sm" id="footer-div">
         <div class="container">
+            <div class="row">
 
-            <!-- Footer Widgets
-            ============================================= -->
-            <div class="footer-widgets-wrap clearfix">
-
-
-            </div><!-- .footer-widgets-wrap end -->
-
-        </div>
-
-        <!-- Copyrights
-        ============================================= -->
-        <div id="copyrights">
-
-            <div class="container clearfix">
-
-                <div class="col_half">
-                    Copyrights &copy; 2014 All Rights Reserved by Canvas Inc.<br>
-                    <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
+                <div class="col-md-3 col-sm-6 paddingtop-bottom">
+                    <div class="" style="text-align: center">
+                        <h6 class="heading7" style="color: white">À PROPOS</h6>
+                        <a href="#"> Foire Aux Questions</a>
+                        <a href="#"> Créer une annonce</a>
+                        <a href="#"> Conditions Générales</a>
+                        <a href="#"> Devenir Professeur</a>
+                        <a href="#"> Qui sommes-nous</a>
+                    </div>
                 </div>
 
-                <div class="col_half col_last tright">
-                    <div class="fright clearfix">
-                        <a href="#" class="social-icon si-small si-borderless si-facebook">
-                            <i class="icon-facebook"></i>
-                            <i class="icon-facebook"></i>
-                        </a>
+                <div class="col-md-3 col-sm-6 paddingtop-bottom">
 
-                        <a href="#" class="social-icon si-small si-borderless si-twitter">
-                            <i class="icon-twitter"></i>
-                            <i class="icon-twitter"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-gplus">
-                            <i class="icon-gplus"></i>
-                            <i class="icon-gplus"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-pinterest">
-                            <i class="icon-pinterest"></i>
-                            <i class="icon-pinterest"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-vimeo">
-                            <i class="icon-vimeo"></i>
-                            <i class="icon-vimeo"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-github">
-                            <i class="icon-github"></i>
-                            <i class="icon-github"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-yahoo">
-                            <i class="icon-yahoo"></i>
-                            <i class="icon-yahoo"></i>
-                        </a>
-
-                        <a href="#" class="social-icon si-small si-borderless si-linkedin">
-                            <i class="icon-linkedin"></i>
-                            <i class="icon-linkedin"></i>
-                        </a>
+                    <div class="text-center center-block">
+                        <h6 class="heading7" style="color: white">SUIVEZ-NOUS</h6>
+                        <br/>
+                        <a href="https://www.facebook.com/taelam"><i id="social-fb"
+                                                                     class="fa fa-facebook-square fa-3x social"></i></a>
+                        <a href="https://twitter.com/taelam"><i id="social-tw"
+                                                                class="fa fa-twitter-square fa-3x social"></i></a>
+                        <a href="https://plus.google.com/+taelam"><i id="social-gp"
+                                                                     class="fa fa-google-plus-square fa-3x social"></i></a>
                     </div>
+                </div>
 
-                    <div class="clear"></div>
+                <div class="col-md-3 col-sm-12">
+                    <h6 class="heading7" style="color: white">TAELAM</h6>
 
-                    <i class="icon-envelope2"></i> info@canvas.com <span class="middot">&middot;</span> <i
-                            class="icon-headphones"></i> +91-11-6541-6369 <span class="middot">&middot;</span> <i
-                            class="icon-skype2"></i> CanvasOnSkype
+                    <p><em>
+                            <small>Taelam est le premier site Marocain de partage de connaissances.
+                                Apprenez sans limites ! Trouvez des professeurs dans tout le Maroc dans des dizaines de
+                                matières différentes.
+                            </small>
+                        </em></p>
+                </div>
+
+                <div class="col-md-3 col-sm-6 ">
+                    <h6 class="heading7" style="color: white">CONTACT</h6>
+
+                    <p><i class="fa fa-newspaper-o"></i> <a href="">Presse</a></p>
+                    <p><i class="fa fa-envelope"></i> <a href="">Contactez-nous</a></p>
                 </div>
             </div>
-        </div><!-- #copyrights end -->
+        </div>
     </footer><!-- #footer end -->
 </div><!-- #wrapper end -->
 
