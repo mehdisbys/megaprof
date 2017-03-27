2705642e0c65d3cae5eaca4df0536dc17b965423
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-03-27 16:50:43 +0100

    faq in progress

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web']], function () {
+    Route::get('/faq', function () {
+        return view('main.faq');
+    });
+
+
 //Lancement
     Route::get('/professeur', 'ListAdvertController@welcomeProfesseur');
 
---
+++ resources/views/main/faq.blade.php
@@ -0,0 +1,229 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div style="margin-top: 60px">
+
+        <div class="question-reponse panel-group col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" href="#collapse1" class="question">Q: Comment trouver un professeur
+                            sur le
+                            site?</a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse reponse">
+                    <div class="panel-body">
+                        A: Pour trouver un professeur, entrez simplement l’activité pour laquelle vous avez besoin d’un
+                        professeur,
+                        la
+                        ville souhaitée (ex: Cuisine, Casablanca). Vous verrez ensuite une liste de tous les professeurs
+                        correspondant à
+                        vos critères. Cliquez sur le nom du professeur pour en savoir plus et le contacter directement.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment se mettre en relation avec le professeur?</div>
+
+            <div class=”reponse”>
+                A: Vous pouvez contacter des offres de professeurs en devenant membres en vous inscrivant sur notre
+                plateforme
+                et c’est gratuit ! Une fois que le professeur a reçu et accepté votre demande, nous vous donnons accès
+                aux
+                détails de ses coordonnées. Nous envoyons également en simultanée vos coordonnées aux professeurs.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment TAELAM Assure-t-il la qualité des professeurs ?</div>
+
+            <div class=”reponse”>
+                A: Le site est une communauté où les professeurs et les élèves sont connectés les uns les autres afin de
+                donner
+                et recevoir des cours. Chaque élève est à la recherche de quelque chose de différent chez un professeur
+                et
+                notre
+                vocation est de vous donner les meilleures options et les outils utiles et nécessaires pour trouver
+                votre
+                professeur idéal.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment organiser une leçon ?</div>
+
+            <div class=”reponse”>
+                A: Vous pouvez convenir d’un arrangement en échangeant avec le professeur via une demande de cours.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: À quel moment dois-je payer ?</div>
+
+            <div class=”reponse”>
+                A: Il est gratuit de chercher, sélectionner et contacter les professeurs de toutes disciplines. Lorsque
+                l’une de
+                vos demandes de cours a été acceptée nous procédons à l’encaissement de X euros qui vous permet ensuite
+                de
+                contacter tous les professeurs sans frais pendant un mois.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Un professeur n’a pas répondu à ma demande, que faire?</div>
+
+            <div class=”reponse”>
+                A: Il se peut qu’un professeur soit temporairement indisponible. Nous contactons les professeurs dès que
+                vous
+                faites une demande de cours. Nous vous recommandons de contacter plus d’un professeur pour avoir le plus
+                de
+                réponses possible. Si vous avez des questions auxquelles nous n’avons pas répondu ou des informations
+                complémentaires que vous souhaitez avoir, merci de nous poser votre question ici.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Combien coûte une heure de cours?</div>
+
+            <div class=”reponse”>
+                A: Chaque professeur est différent, l’heure peut ainsi varier selon le professeur, le niveau, le
+                contenu, la
+                structure. Sur le profil de chaque professeur, vous pouvez voir le prix de l’heure qu’il propose. Sur le
+                profil,
+                vous avez aussi une liste importante d’informations comme son expérience, ses qualifications, ses atouts
+                etc.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Où se dérouleront les cours?</div>
+
+            <div class=”reponse”>
+                A: Les cours auront lieu où vous le souhaitez, le choix vous revient à vous et au professeur. Si vous
+                décidez
+                que le cours se passe à la maison, vous pouvez simplement le dire au professeur via la boîte de
+                messagerie.
+                Beaucoup de professeurs acceptent que les cours se déroulent dans un lieu public comme une librairie, un
+                café ou
+                une bibliothèque. Plusieurs professeurs également se sont spécialisés dans les cours par internet. C’est
+                comme
+                vous voulez !
+            </div>
+        </div>
+
+        D’autres questions?
+
+        Contactez-nous !
+        Rejoignez-nous ! « Trouvez votre professeur » !
+
+        Questions fréquemment posées par les professeurs :
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Combien coûte l’inscription sur le site TAELAM En tant que professeur ?</div>
+
+            <div class=”reponse”>
+                A: L’inscription et les services fournis par TAELAM sont gratuits pour les professeurs.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment serais-je averti par les demandes des élèves ?</div>
+
+            <div class=”reponse”>
+                A: Nous vous enverrons un mail comprenant les demandes de cours des élèves en vous invitant à vous
+                connecter
+                à
+                votre espace afin de répondre à celles-ci dès que possible pour vous. Si un professeur reçoit des
+                demandes
+                qu’il
+                laisse sans réponse, ses annonces ne seront plus visibles sur le site, ceci dans un souci de toujours
+                avoir
+                des
+                annonces à jour et actualisées.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Quand puis-je commencer à donner un cours?</div>
+
+            <div class=”reponse”>
+                A: Dès que vous avez accepté la demande d’un élève, celui-ci peut voir vos coordonnées et vous
+                contacter.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment changer mon profil?</div>
+
+            <div class=”reponse”>
+                A: Dans votre espace dédié lors de votre inscription, vous pouvez changer les informations ou les
+                conditions
+                de
+                votre profil à tout moment.
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Comment fonctionne le système de commentaires ?</div>
+
+            <div class=”reponse”>
+                A: Après le premier cours ou quelques cours, nous invitons vos élèves à faire part de leur expérience en
+                laissant un commentaire . Notre vocation à travers cette plateforme est d’avoir une communauté
+                partageant un
+                savoir, une connaissance ou une compétence de qualité, pour cela plus il y a de commentaires renseignés
+                plus
+                votre profil sera attractif.
+            </div>
+        </div>
+
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: De quelles qualifications ai-je besoin pour être professeur ?</div>
+
+            <div class=”reponse”>
+                A: Notre site attire une très large gamme de disciplines, le niveau de qualification demandé dépendra
+                donc
+                de
+                ces activités et des attentes des élèves, nous laissons le choix à l’élève de trouver son professeur en
+                fonction
+                de ses critères, nous ne limitons pas le professeur à des qualifications mais à ce qu’il peut apporter à
+                l’élève,
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Est-ce que vous employez des professeurs?</div>
+
+            <div class=”reponse”>
+                A: Non, tous les professeurs travaillent à leur compte, sans intermédiaires,
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Je suis un travailleur étranger, puis-je être un professeur?</div>
+
+            <div class=”reponse”>
+                A: Si votre titre de séjour vous permet de travailler dans le pays, vous pouvez bien sûr devenir
+                professeur,
+            </div>
+        </div>
+
+        <div class=”question-reponse”>
+            <div class=”question”>Q: Quel tarif demandé ?</div>
+            <div class=”reponse”>
+                A: Le tarif va dépendre de plusieurs critères comme : Du type d’activité, du niveau de l’enseignement,
+                et de
+                votre région. Et enfin, vous devez voir quels prix proposent les autres profs proches de chez vous pour
+                la
+                même
+                activité ou des activités similaires.
+            </div>
+        </div>
+    </div>
+@endsection
