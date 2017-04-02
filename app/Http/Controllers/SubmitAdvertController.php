89cb5e0a6680674361e1702c83e255aab340ffbf
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-04-02 19:29:39 +0100

    dont publish before reviewing

--- app/Http/Controllers/SubmitAdvertController.php
+++ app/Http/Controllers/SubmitAdvertController.php
@@ -257,6 +257,7 @@ class SubmitAdvertController extends Controller
         $request->input('advert_id');
         $advert              = Advert::find(session('advert_id') ?? $request->input('advert_id'));
         $advert->approved_at = NULL;
+        $advert->publish();
 
         event(new ProfCreatedAdvert($advert));
 
