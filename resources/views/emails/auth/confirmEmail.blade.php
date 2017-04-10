6ada77f3214204443a261bd3b300d87fa0495603
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-04-10 16:24:44 +0100

    Auth emails updated

--- resources/views/emails/auth/confirmEmail.blade.php
+++ resources/views/emails/auth/confirmEmail.blade.php
@@ -1,23 +1,23 @@
 @extends('emails.master')
 @section('content')
 
-    <h2>{{$name}}, bienvenue sur TAELAM ! – Confirmation de votre inscription</h2>
+    <h2>{{$name}}, Bienvenue sur TAELAM ! – Confirmation de votre inscription</h2>
     <br>
 
     <p>Bonjour {{$name}},</p>
 
     <br>
 
-    <p>Pour commencer à bénéficier de nos services, merci de confirmer la création de votre compte en vous cliquant sur le bouton suivant :</p>
+    <p>Pour commencer à bénéficier de nos services, merci de confirmer la création de votre compte en cliquant sur le bouton suivant :</p>
     <div class="" style="text-align: center">
         <a href="{{$link}}" class="btn btn-success">Se Connecter</a>
     </div>
     <br>
-    <p>Nous vous remercions de votre confiance,suivez-nous sur les réseaux sociaux pour être au courant de l'actualité
-        Taelam.</p>
+    <p>Nous vous remercions de votre confiance, suivez-nous sur les réseaux sociaux pour être au courant de l'actualité
+ Taelam.</p>
     <br>
     <p>
-        A très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
+        À très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
     <br>
 
     <p>L'Équipe Taelam</p>
--- resources/views/emails/auth/resetPassword.blade.php
+++ resources/views/emails/auth/resetPassword.blade.php
@@ -9,14 +9,14 @@
     <br>
 
     <p>Vous avez demandé une réinitialisation de votre mot de passe,
-        Pour en créer un nouveau, merci de cliquer <a href="{{$link}}">ici</a></p>
+        Pour en créer un nouveau, merci de cliquer sur <a href="{{$link}}">ici</a></p>
 
     <p>Vous pourrez à nouveau accéder à l’ensemble de nos services.</p>
 
     <p>Veuillez ignorer cet email si vous n'avez pas demandé de réinitialisation de votre mot de passe.</p>
     <br>
 
-    <p>A très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
+    <p>À très bientôt sur <a href="http://www.taelam.com">Taelam</a></p>
 
     <br>
 
