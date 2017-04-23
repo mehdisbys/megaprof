5e803fbeab252173073b0bc427d043cd6a6867b2
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-04-24 00:15:40 +0300

    dropbox saving backups

--- saveToDropBox.php
+++ saveToDropBox.php
@@ -5,7 +5,7 @@ $localArchiveName = "db_backup.sql.gz";
 
 $archiveNameOnDropBox = 'backup-' . time() . '.tar.gz.gpg';
 
-$command = "gpg --encrypt --recipient 'Dropbox Key' --batch --yes {$localArchiveName} ; curl -X POST https://content.dropboxapi.com/2/files/upload \
+$command = "mysqldump -u root megaprof > db_backup.sql ; tar -czvf {$localArchiveName} db_backup.sql ; gpg --encrypt --recipient 'Dropbox Key' --batch --yes {$localArchiveName} ; curl -X POST https://content.dropboxapi.com/2/files/upload \
 --header \"Authorization: Bearer AvhrHr29xIAAAAAAAAAABy_NL_Hryd-uJWu6xCW8XerE7UqpaXNzuXydY2GBYwle\" \
 --header 'Dropbox-API-Arg: {\"path\": \"/{$archiveNameOnDropBox}\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}' \
 --header \"Content-Type: application/octet-stream\" \
