24ce6620513d88123ff7875751d67a05a26419d2
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-04-26 15:09:16 +0100

    Add facebook

--- resources/views/layouts/__master.blade.php
+++ resources/views/layouts/__master.blade.php
@@ -140,7 +140,7 @@
                     <div class="text-center center-block">
                         <h6 class="heading7" style="color: white">SUIVEZ-NOUS</h6>
                         <br/>
-                        <a href="https://www.facebook.com/taelam"><i id="social-fb"
+                        <a href="https://www.facebook.com/taelamOfficiel"><i id="social-fb"
                                                                      class="fa fa-facebook-square fa-3x social"></i></a>
                         <a href="https://twitter.com/taelam"><i id="social-tw"
                                                                 class="fa fa-twitter-square fa-3x social"></i></a>
