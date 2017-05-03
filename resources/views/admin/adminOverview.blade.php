f8b6180d5280eb0624db3246e432f9f3188b8419
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-05-03 11:24:59 +0100

    Add smartlook to admin dashboard

--- resources/views/admin/adminOverview.blade.php
+++ resources/views/admin/adminOverview.blade.php
@@ -9,10 +9,11 @@
         <h2 class="col-md-6 border1px"><a href="/admin/lister-utilisateurs">Utilisateurs : {{$usersCount}}</a></h2>
         <h2 class="col-md-6 border1px"><a href="/annonces-validees">Annonces validées : {{$approvedAdvertsCount}}</a></h2>
         <h2 class="col-md-12 border1px">
-            <a class="col-md-3" href="https://analytics.google.com/analytics/web/"><i class="fa fa-line-chart"></i></a>
-            <a class="col-md-3" href="https://www.facebook.com/taelamOfficiel"><i class="fa fa-facebook-square"></i></a>
-            <a class="col-md-3" href="https://twitter.com/taelam_officiel"><i class="fa fa-twitter-square"></i></a>
-            <a class="col-md-3" href="https://plus.google.com/u/1/115934799609055669898"><i class="fa fa-google-plus-square"></i></a>
+            <a class="col-md-2" href="https://analytics.google.com/analytics/web/"><i class="fa fa-line-chart"></i></a>
+            <a class="col-md-2" href="https://www.facebook.com/taelamOfficiel"><i class="fa fa-facebook-square"></i></a>
+            <a class="col-md-2" href="https://twitter.com/taelam_officiel"><i class="fa fa-twitter-square"></i></a>
+            <a class="col-md-2" href="https://plus.google.com/u/1/115934799609055669898"><i class="fa fa-google-plus-square"></i></a>
+            <a class="col-md-2" href="https://www.smartlook.com/app/dashboard"><i class="fa fa-eye"></i></a>
         </h2>
     </div>
 @endsection
