fc72e35fee1dccba2ae7fe4c52a1a7a5947fd9a0
mehdisbys <mehdi.souihed@sainsburys.co.uk>
2017-06-05 16:40:05 +0100

    Email errors to my mailbox

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -30,6 +30,14 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
+        if ($e instanceof \Exception) {
+            // emails.exception is the template of your email
+            // it will have access to the $error that we are passing below
+            Mail::send('emails.exception', ['error' => $e->getMessage()], function ($m) {
+                $m->to('mehdi.souihed@gmail.com', 'Mehdi')->subject('Erreur sur Taelam');
+            });
+        }
+
         return parent::report($e);
     }
 
---
+++ resources/views/emails/exception.blade.php
@@ -0,0 +1,9 @@
+@extends('emails.master')
+
+@section('content')
+
+    <h3>Erreur sur Taelam :</h3>
+
+    <code>{{$error}}</code>
+
+@endsection
