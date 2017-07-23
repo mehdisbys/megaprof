6213c3267cf3876c6dc811cacbc077cb640ce4be
Mehdi <mehdi.souihed@gmail.com>
2017-07-23 13:11:36 +0100

    remove message that says website is not ready

---
+++ resources/views/partials/postit.blade.php
@@ -0,0 +1,9 @@
+<div class="quote-container" >
+    <i class="pin"></i>
+    <blockquote class="note yellow">
+        Le site n'est pas encore ouvert aux élèves car nous sommes en train de <strong>sélectionner les meilleurs professeurs pour vous</strong>, mais
+        vous pouvez nous aider en remplissant les champs ci-dessous, merci !
+        <cite class="author">Équipe Taelam</cite>
+    </blockquote>
+
+</div>
