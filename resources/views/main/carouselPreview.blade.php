679a9d06be8935a911b719641ce52a034c0f0089
Mehdi <mehdi.souihed@gmail.com>
2017-08-06 09:18:28 +0300

    Smaller leaner carousel preview

--- resources/views/main/carouselPreview.blade.php
+++ resources/views/main/carouselPreview.blade.php
@@ -6,12 +6,12 @@
                     <img src="{{ $advert->getAvatar() }}" alt="avatar">
                     <h3><a href="#" class="center">{{ucfirst(strtolower($advert->user->firstname ))}}</a></h3>
                     <ul class="iconlist-info">
-                        @if($advert->can_webcam)
-                            <li>
-                                <i class="fa fa-skype"></i>
-                                <small>Donne des cours par webcam</small>
-                            </li>
-                        @endif
+                        {{--@if($advert->can_webcam)--}}
+                            {{--<li>--}}
+                                {{--<i class="fa fa-skype"></i>--}}
+                                {{--<small>Donne des cours par webcam</small>--}}
+                            {{--</li>--}}
+                        {{--@endif--}}
                         @if($advert->can_travel)
                             <li>
                                 <i class="fa fa-home"></i>
@@ -37,22 +37,13 @@
                 </div>
             </div>
             <div class="single-view-info-author">
-                <h1 class="single-view-title"><a href="/{{$advert->slug}}">{{ str_limit(ucfirst($advert->title), 75) }}</a></h1>
+                <h3 class="single-view-title-carousel"><a href="/{{$advert->slug}}">{{ str_limit(ucfirst($advert->title), 95) }}</a></h3>
 
                 <div class="pull-left">
                     @foreach($advert->subjectsPerAd as $subject)
                         <div class="label label-info">{{ \App\Models\SubSubject::find($subject->subject_id)->name}}</div>
                         <div class="clearfix"></div>
                     @endforeach
-                </div>
-
-                <div class="pull-right">
-                    <?php $advertLevels = json_decode(\App\Models\SubjectsPerAdvert::getLevelsPerAdvert($advert->id)[0], true); ?>
-                    @if(is_array($advertLevels))
-                        @foreach($advertLevels as $level_id)
-                            <div class="label label-primary">{{\App\Models\SubLevel::find($level_id)->name}}</div>
-                        @endforeach
-                    @endif
                 </div>
 
                 <div class="col-md-12 topmargin-sm">
