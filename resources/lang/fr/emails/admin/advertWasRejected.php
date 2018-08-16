9ac88b122437c4094c7b638dd4c438c4a5f80d70
Mehdi <mehdi.souihed@gmail.com>
2018-08-16 21:41:58 +0100

    translate files IV

---
+++ resources/lang/fr/emails/admin/advertWasRejected.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'thankInterest' => "Merci de votre intérêt pour Taelam, le site dédié aux cours particuliers.",
+    'advert' => "Annonce :",
+    'moderatedAdvert' => "Votre annonce a été modérée par l'un de nos administrateurs.",
+    'cantPublish' => "Nous ne pouvons pas la publier pour le moment pour la raison suivante :",
+    'connect' => "Connectez-vous sur",
+    'modifyYourAdvert' => " pour la modifier afin qu'elle soit publiée au plus vite",
+    'seeYou' => "À très bientôt sur Taelam",
+    'fromTheTeam' => "L'Équipe Taelam",
+];
--- resources/views/emails/admin/advertWasRejected.blade.php
+++ resources/views/emails/admin/advertWasRejected.blade.php
@@ -1,26 +1,30 @@
 @extends('emails.master')
 @section('content')
 
-    <p>Bonjour {{$name }},</p>
+    <p>{{$name }}</p>
 
     <br>
-    <p>Merci de votre intérêt pour Taelam, le site dédié aux cours particuliers.</p>
+    <p>@lang('emails/admin/advertWasRejected.thankInterest')</p>
     <br>
-    <p>Votre annonce : {{$advert->title}} a été modérée par l'un de nos administrateurs.</p>
+    <p>@lang('emails/admin/advertWasRejected.advert')</p>
+    <p>{{$advert->title}}</p>
+    <p>@lang('emails/admin/advertWasRejected.moderatedAdvert')</p>
     <br>
 
-    <p>Nous ne pouvons pas la publier pour le moment pour la raison suivante : </p>
+    <p>@lang('emails/admin/advertWasRejected.cantPublish') </p>
 
     <p>{{$messageAdmin}}</p>
 
 
 
-    <p>Connectez-vous sur <a href="https://www.taelam.com/mon-compte">Taelam</a> pour la modifier afin qu'elle soit publiée au plus vite.</p>
+    <p>
+        <a href="https://www.taelam.com/mon-compte">@lang('emails/admin/advertWasRejected.connect')</a>@lang('emails/admin/advertWasRejected.modifyYourAdvert')
+    </p>
 
     <br>
 
-    <p>À très bientôt sur <a href="https://www.taelam.com">Taelam</a></p>
+    <p><a href="https://www.taelam.com">@lang('emails/admin/advertWasRejected.seeYou')</a></p>
     <br>
 
-    <p>L'Équipe Taelam</p>
+    <p>@lang('emails/admin/advertWasRejected.fromTheTeam')</p>
 @stop
