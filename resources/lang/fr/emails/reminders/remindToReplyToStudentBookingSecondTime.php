3b44078b8093ec99d3f09e215f6c1bb981cb6a34
Mehdi <mehdi.souihed@gmail.com>
2018-08-30 20:09:59 +0100

    translate files

---
+++ resources/lang/fr/emails/reminders/remindToReplyToStudentBookingSecondTime.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'oneDayLeft' => "Vous avez reçu une réservation d'annonce hier, il vous reste un jour pour répondre !",
+    'warning' => "<strong>A titre d'information : toute réservation qui n'aura pas reçu de réponse sous 48h sera automatiquement annulée et vous n'aurez pas la possibilité de contacter l'élève.</strong>",
+    '' => "Si vous êtes disponible pour donner un cours, <a href=\"https://taelam.com/mon-compte\"> connectez-vous à votre tableau de bord </a>et acceptez la demande de votre élève.",
+    'unlockDetails' => "Après avoir accepté la réservation, <strong>les coordonnées telles que email de l'élève et numéro de téléphone vous seront communiqués.</strong>",
+    'ifNotAvailable' => "Si vous n'êtes plus disponible pour donner cours ou pour quelque autre raison vous ne pouvez pas répondre positivement à l'élève, <a href=\"https://taelam.com/mon-compte\">connectez-vous à votre tableau de bord et refusez la demande </a>. En montrant votre professionalisme vous serez plus susceptible d'être contacté(e) à l'avenir et votre réputation sera impeccable.",
+    'myAccount' => "Aller sur mon compte Taelam",
+    'team' => "L’Equipe TAELAM",
+];
--- resources/views/emails/reminders/remindToReplyToStudentBooking.blade.php
+++ resources/views/emails/reminders/remindToReplyToStudentBooking.blade.php
@@ -2,19 +2,19 @@
 @section('content')
 
 
-    <p>{{$user->firstname}},</p>
+    <p>{{$user->firstname}}</p>
 
-    <p>Bonne nouvelle, vous avez reçu une réservation d'annonce récemment !</p>
+    <p>@lang('reminders/remindToReplyToStudentBooking.goodNews')</p>
 
-    <p>Si vous êtes disponible pour donner un cours, <a href="https://taelam.com/mon-compte"> connectez-vous à votre tableau de bord </a>et acceptez la demande de votre élève.</p>
+    <p>@lang('reminders/remindToReplyToStudentBooking.connectToYourAccount')</p>
 
-    <p>Après avoir accepté la réservation, <strong>les coordonnées telles que email de l'élève et numéro de téléphone vous seront communiqués.</strong></p>
-
-
-    <p>Si vous n'êtes plus disponible pour donner cours ou pour quelque autre raison vous ne pouvez pas répondre positivement à l'élève, <a href="https://taelam.com/mon-compte">connectez-vous à votre tableau de bord et refusez la demande </a>. En montrant votre professionalisme vous serez plus susceptible d'être contacté(e) à l'avenir et votre réputation sera impeccable.</p>
+    <p>@lang('reminders/remindToReplyToStudentBooking.unlockDetails')</p>
 
 
-    <p> <em>A titre d'information : toute réservation qui n'aura pas reçu de réponse sous 48h sera automatiquement annulée et vous n'aurez pas la possibilité de contacter l'élève.</em></p>
+    <p>@lang('reminders/remindToReplyToStudentBooking.ifNotAvailable')</p>
+
+
+    <p> @lang('reminders/remindToReplyToStudentBooking.autoCancel')</p>
 
     <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center"
            style="margin: auto;">
@@ -23,12 +23,13 @@
                 <a href="https://taelam.com/mon-compte"
                    style="background: #fd875e; border: 15px solid #fd875e; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;"
                    class="button-a">
-                    <span style="color:#ffffff;" class="button-link">Aller sur mon compte Taelam</span>
+                    <span style="color:#ffffff;"
+                          class="button-link">@lang('reminders/remindToReplyToStudentBooking.myAccount')</span>
                 </a>
             </td>
         </tr>
     </table>
 
 
-    <p>L’Equipe TAELAM </p>
+    <p>@lang('reminders/remindToReplyToStudentBooking.team')</p>
 @stop
