dc195a8b33fd67ea7bca9cc8532392f227976ddd
Mehdi <mehdi.souihed@gmail.com>
2018-09-09 19:26:59 +0100

    file translation continued

--- resources/lang/fr/emails/requests/bookingRequestStudent.php
+++ resources/lang/fr/emails/requests/bookingRequestStudent.php
@@ -2,6 +2,6 @@
 return [
     'confirm' => "Nous vous confirmons que votre demande de cours a bien été envoyée.",
     'notify' => "Nous vous enverrons un email lorsque le professeur aura répondu à votre demande.",
-    'seeYouSoon' => "",
+    'seeYouSoon' => "À très bientôt sur Taelam",
     'team' => "L'Équipe Taelam",
 ];
---
+++ resources/lang/fr/layouts/master.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'pageTitle' => "Taelam - Cours particuliers au Maroc : Soutien scolaire, Concours,
+            Mathématiques, Langues...",
+    'title' => "Cours particuliers, cours à domicile, soutien scolaire en mathématiques, anglais, arabe, musique et bien d'autres matières dans tout le Maroc",
+    'metaTitle' => "Taelam - La plateforme des professeurs particuliers",
+    'taelam' => "Taelam",
+    'findATeacher' => "Trouvez votre professeur pour des cours particuliers",
+    'goHome' => "Revenir à l'accueil de Taelam",
+    'logoAlt' => "Cours particuliers avec Taelam",
+    'createAdvert' => "Créer une annonce",
+    'myAccount' => "Mon Compte",
+    'logout' => "Se déconnecter",
+    'help' => "Aide",
+    'connect' => "Se connecter",
+    'register' => "S'inscrire",
+    'giveLessons' => "Donner des cours",
+    '' => "",
+];
