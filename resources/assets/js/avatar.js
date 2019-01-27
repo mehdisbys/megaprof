c9b800caed652cce91a50cd82d7ee9162ef1e9b2
Mehdi <mehdi.souihed@gmail.com>
2019-01-27 16:29:47 +0000

    avatar letter iteration

--- resources/assets/js/avatar.js
+++ resources/assets/js/avatar.js
@@ -11,7 +11,7 @@
     function LetterAvatar(name, size) {
 
         name = name || '';
-        size = size || 60;
+        size = size || 75;
 
         var colours = [
                 "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",
